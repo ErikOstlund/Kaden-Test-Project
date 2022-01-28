0a2d720162ff25a50636bebd8fcbee49c3245aec
ErikOstlund <eostlund74@gmail.com>
2022-01-28 15:56:01 -0600

    Added some JS

---
+++ Javascript/core.js
@@ -0,0 +1,32 @@
+console.log('I am from the core.js file!');
+
+// Assigning values
+const name = 'erik';
+
+// Value type
+// string: 'hello'
+// number: 678
+// boolean: true or false
+// array: ['thing1', 67, true, red, green]
+// function: {}
+
+const num1 = 4;
+const num2 = 8;
+
+const sum = num1 + num2;
+
+console.log(sum);
+
+//Function
+
+const f1 = 10;
+const f2 = 7;
+
+// created function
+function multiply(param1, param2) {
+	const product = param1 * param2;
+	return product;
+}
+
+// calls the function
+console.log(multiply(f1, f2));
